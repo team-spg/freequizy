05a1daed896e8e75371f4d0b0d322735feb24906
Islomjon Akhmadjonov <politurin@mail.ru>
2018-05-13 14:59:36 +0500

    Add files via upload

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,10 +1,10 @@
-var array = new Array("img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png", "img/6.png");
+var array = new Array("img/1.png", "img/2.png", "img/3.png", "img/4.png", "img/5.png");
 var count = 0;
 function slideshow(){
 	var baner = document.getElementById("baner");
 	baner.src = array[count];
 	count++;
-	if(count == 6)
+	if(count == 5)
 		count = 0;
 }
 var interval = setInterval("slideshow()", 2000);
