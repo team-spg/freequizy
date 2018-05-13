0574a4e42d78b87c05432f5059237f08752b067d
ozodkhushnudov <39234642+ozodkhushnudov@users.noreply.github.com>
2018-05-13 11:58:50 +0300

    Add files via upload

---
+++ information.php
@@ -0,0 +1,32 @@
+<?php 
+	include("widgets/server.php");
+	
+	if(empty($_SESSION['username'])){
+		header('location: sign_in.php');
+	}
+?>
+
+<html>
+	<head>
+		<title>quiz</title>
+		<meta charset = "utf-8"/>
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<script type="text/javascript" src="jquery/jquery-3.0.0.min.js"></script>
+		<script type="text/javascript" src="js/loader.js"></script>
+		<script type="text/javascript" src="js/slideshow.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div id = "load"></div>
+			<div id="content">
+				<?php include("widgets/head.php"); ?>
+				<?php include("widgets/baner.php"); ?>
+				<?php include("widgets/info.php"); ?>
+				<?php include("widgets/team.php"); ?>
+				<?php include("widgets/footer.php"); ?>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ sign_in.php
@@ -0,0 +1,26 @@
+<?php include("widgets/server.php"); ?>
+<html>
+	<head>
+		<title>quiz</title>
+		<meta charset = "utf-8"/>
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0"/>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<script type="text/javascript" src="jquery/jquery-3.0.0.min.js"></script>
+		<script type="text/javascript" src="js/loader.js"></script>
+		<script type="text/javascript" src="js/slideshow.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+		<script type="text/javascript" src="js/login.js"></script>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div id = "load"></div>
+			<div id="content">
+				<?php include("widgets/head.php") ?>
+				<?php include("widgets/baner.php") ?>
+				<?php include("widgets/login.php") ?>
+				<?php include("widgets/team.php") ?>
+				<?php include("widgets/footer.php") ?>
+			</div>
+		</div>
+	</body>
+</html>
